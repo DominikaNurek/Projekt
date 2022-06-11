5ed46afdacb6d935609959b0e4fdeae97aba9fd4
DominikaNurek <106488136+DominikaNurek@users.noreply.github.com>
2022-06-12 00:38:49 +0200

    Update Owad.java

--- Projekt/src/main/java/Owad.java
+++ Projekt/src/main/java/Owad.java
@@ -1,24 +1,31 @@
-public class Owad {
+public abstract class Owad {
     private int [][] wspolrzedne;
+
+    protected int szerszenieWokol;
+    protected int pszcczolyWokol;
     protected int liczbaGatunku;  //zmienna, która zlicza ile jest owadow danego gatunku
     protected byte szybkoscPoruszania;
 
     protected void ruch (byte szybkoscPoruszania){
 
     }
-
-    protected void walka(){
-
+    protected void sprawdzOkolice(){
+        
     }
 
     public int[][] getWspolrzedne() {
         return wspolrzedne;
     }
-
     public void setWspolrzedne(int[][] wspolrzedne) {
         this.wspolrzedne = wspolrzedne;
     }
     public byte getSzybkoscPoruszania() {
         return szybkoscPoruszania;
+    }
+    public int getLiczbaGatunku() {
+        return liczbaGatunku;
+    }
+    public void setLiczbaGatunku(int liczbaGatunku) {
+        this.liczbaGatunku = liczbaGatunku;
     }
 }
