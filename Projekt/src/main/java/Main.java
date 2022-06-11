5bb0de7259c99cb1505b746d510544279513dd57
DominikaNurek <106488136+DominikaNurek@users.noreply.github.com>
2022-06-12 00:27:55 +0200

    Update Main.java
    Usunęłam setery dla liczby pszczół i szerszeni, bo skoro one są podawane przez użytkownika, to chyba tak jest łatwiej, szczególnie, że to wartości wejściowe, które się chyba nie zmieniają, a to w zmiennej liczbaGatunku jest zapisana aktualna liczba pszczół i szerszeni. No chyba, że chcemy, by tu były też te zmienne wartości?

--- Projekt/src/main/java/Main.java
+++ Projekt/src/main/java/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
-    public void main((String[] args){
+    private static int liczbaPszczolNaWejsciu;
+    private static int liczbaSzerszeniNaWejsciu;
+    private static double szerokoscLaki = 20;
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbe pszczol mniejsza od " + szerokoscLaki*szerokoscLaki + ": ");
+        liczbaPszczolNaWejsciu = scan.nextInt();
+
+        while(liczbaPszczolNaWejsciu<=0 || liczbaPszczolNaWejsciu>=0.8*(szerokoscLaki*szerokoscLaki)){
+            if(liczbaPszczolNaWejsciu<=0){
+                System.out.println("Podana liczba jest nieprawidlowa, podaj dodatnia wartosc:");
+            }
+            else
+                System.out.println("Podana liczba jest nieprawidlowa, podaj mniejsza liczbe:");
+
+            liczbaPszczolNaWejsciu = scan.nextInt();
+        }
+
+        System.out.print("Podaj liczbe szerszeni mniejsza od " + (szerokoscLaki*szerokoscLaki-liczbaPszczolNaWejsciu) + ": ");
+        liczbaSzerszeniNaWejsciu = scan.nextInt();
+
+        while(liczbaSzerszeniNaWejsciu<=0 || liczbaSzerszeniNaWejsciu>=szerokoscLaki*szerokoscLaki-liczbaPszczolNaWejsciu){
+            if(liczbaSzerszeniNaWejsciu<=0){
+                System.out.println("Podana liczba jest nieprawidlowa, podaj dodatnia wartosc:");
+            }
+            else
+                System.out.println("Podana liczba jest nieprawidlowa, podaj mniejsza liczbe:");
+
+            liczbaSzerszeniNaWejsciu = scan.nextInt();
+        }
+    }
+
+    public int getLiczbaPszczolNaWejsciu() {
+        return liczbaPszczolNaWejsciu;
+    }
+
+    public int getLiczbaSzerszeniNaWejsciu() {
+        return liczbaSzerszeniNaWejsciu;
+    }
+
+    public void wyswietlLake(){
 
     }
 }
