eba8cffccd35695c61580b1a638bc64eebd5a4c5
DominikaNurek <106488136+DominikaNurek@users.noreply.github.com>
2022-06-12 12:32:04 +0200

    Update Ul.java

--- Projekt/src/main/java/Ul.java
+++ Projekt/src/main/java/Ul.java
@@ -1,9 +1,8 @@
 public class Ul {
     private int sumaNektaru = 0;
-    public int iloscPszczol;
+    public int liczbaPszczol;
 
-    private void liczNektar(int zebranyNektar){
-        sumaNektaru += zebranyNektar;
+    private void liczNektar(){
         if(sumaNektaru >= 50){
             tworzeniePszczoly();
             sumaNektaru -= 50;
@@ -12,4 +11,8 @@ public class Ul {
     private void tworzeniePszczoly(){
 
     }
+    public void setSumaNektaru(int nowyNektar) {
+        sumaNektaru += nowyNektar;
+    }
+
 }
