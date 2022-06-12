8f5d8f34b131b11b5d259d153e9e970243cacc3e
DominikaNurek <106488136+DominikaNurek@users.noreply.github.com>
2022-06-12 12:28:44 +0200

    Update Pszczola.java

--- Projekt/src/main/java/Pszczola.java
+++ Projekt/src/main/java/Pszczola.java
@@ -1,13 +1,19 @@
 public class Pszczola extends Owad{
     private int zebranyNektar;
-    Laka object = new Laka();
-    public int[][] spisPszczol = object.spisPszczol;
-    
+
     public Pszczola(){
         this.szybkoscPoruszania = 2;
     }
 
     public int getZebranyNektar() {
         return zebranyNektar;
+    }
+
+    public void setZebranyNektar(int nektar) {
+        zebranyNektar += nektar;
+    }
+
+    public void zerujNektar(){
+        zebranyNektar = 0;
     }
 }
